429c736898d5646b8386f79be128baabe354807f
cgnz0r <cgnz0r@gmail.com>
2020-07-25 19:28:10 +0700

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Order />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Order from './components/Order.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Order
   }
 }
 </script>
---
+++ src/components/Order.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Order Component</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Order',
+}
+</script>
